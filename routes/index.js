38aa2807a7f5da024104b91b22c94d07f30e7ab8
Antarctic-penguin <jamesantarcticpenguin@gmail.com>
2023-10-09 00:00:48 +0800

    To-do List x Sequelize

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+
+// 準備引入路由模組
+const users = require('./modules/users')
+const home = require('./modules/home')
+const todos = require('./modules/todo')
+const auth = require('./modules/auth')
+
+const { authenticator } = require('../middleware/auth')
+
+router.use('/todos', authenticator, todos)
+router.use('/users', users)
+router.use('/auth', auth)
+router.use('/', authenticator, home)
+
+// 匯出路由器
+module.exports = router
